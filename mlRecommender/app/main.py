1522368b1f8362131c357ef188de507e00831037
Nishkarshs1 <nishkarshsharma384@gmail.com>
2026-06-07 23:40:34 +0530

    Configure CORS for Vercel

--- mlRecommender/app/main.py
+++ mlRecommender/app/main.py
@@ -64,7 +64,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://elysiumsvd.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
